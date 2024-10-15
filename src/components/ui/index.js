1af9c235f50623d2b21b19886cf86e4a214a7277
Mazin Emad <124470200+Mazin-emad@users.noreply.github.com>
2024-10-15 19:14:50 +0300

    Updates

--- src/components/ui/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Button } from "./Button/Button";
-export { default as Tabs } from "./tabs/Tabs";
-export { default as Tab } from "./tabs/Tab";
-export { default as Model } from "./Model/Model";
--- src/services/apis/categories.api.js
+++
@@ -1,6 +0,0 @@
-import axiosApi from "./axiosApi";
-
-export const getCategory = async () => {
-  const { data } = await axiosApi.get("/categories/");
-  return data;
-};
